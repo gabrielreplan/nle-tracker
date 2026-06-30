9557fb3cea5e982ac2452c14bb3a06ee517227dd
gabrielgladion <gaboreplan4@gmail.com>
2026-06-30 21:00:48 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump this on every deploy so clients pick up the new version automatically.
-const VERSION = 'v3';
+const VERSION = 'v4';
 const CACHE = `nle-tracker-${VERSION}`;
 
 const ASSETS = [
